2b625158d4b9b436794108bab5befd5d46605841
Li Guanlong <liguanlongclivian@outlook.com>
2018-10-04 15:41:35 +0800

    updated project framework

--- CMS/callcentre/models.py
+++ CMS/callcentre/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Incident(models.Model):
+    incident_time = models.CharField(max_length=100)
+    incident_location = models.CharField(max_length=100)
+    incident_category = models.CharField(max_length=100)
+    incident_description = models.CharField(max_length=400)
