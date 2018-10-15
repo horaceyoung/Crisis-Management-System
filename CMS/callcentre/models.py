a41c7c39bcd15ef3528440444f0c62226b5c5bfa
Emil Luusua <emilluusua@Borta.local>
2018-10-15 23:18:29 +0800

    Adjusted database to utilize Enums and created entry (and Enum) for region

--- CMS/callcentre/models.py
+++ CMS/callcentre/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from utilities.incidentstatus import IncidentStatus
+from utilities.incidenttype import IncidentType
+from utilities.region import Region
 
 
 class Incident(models.Model):
-    incident_time = models.CharField(max_length=100)
+    incident_time = models.DateTimeField()
     incident_location = models.CharField(max_length=100)
-    incident_category = models.CharField(max_length=100)
+    incident_region = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in Region])
+    incident_category = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentType])
+    incident_status = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentStatus])
     incident_description = models.CharField(max_length=400)
