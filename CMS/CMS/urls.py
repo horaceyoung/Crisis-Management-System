a53590540be7d77a991f583b024a12d5c3c5c911
Yuehann <zyh990424@gmail.com>
2018-10-16 23:38:15 +0800

    workspace and url

--- CMS/CMS/urls.py
+++ CMS/CMS/urls.py
@@ -6,7 +6,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('map.urls', namespace='map')),
     path('callcentre/', include('callcentre.urls', namespace='callcentre')),
+<<<<<<< HEAD
     path('department/', include('department.urls', namespace='department')),
     path('statustrack/', include('statusTrack.urls', namespace='statustrack')),
     path('infodistribution/', include('infodistribution.urls', namespace='infodistribution')),
 ]
+=======
+    # path('department/', include('department.urls', namespace='department')),
+    path('statustrack/', include('statusTrack.urls', namespace='statustrack')),
+    path('infodistribution/', include('infodistribution.urls', namespace='infodistribution')),
+]
+>>>>>>> e1a117cd58a5e32619dfefd31599f1712a0a7f8e
