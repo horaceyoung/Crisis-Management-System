e5d2f1c5019d4e7b33a580fdc4dc7a8cfa54dcdc
mengyan-FU <mfu003@e.ntu.edu.sg>
2018-10-16 23:00:54 +0800

    change

--- CMS/callcentre/views.py
+++ CMS/callcentre/views.py
@@ -3,6 +3,6 @@ from .models import Incident
 
 
 def callcentre_home(request):
-    all_incidents = Incident.objects.all()
+    all_incidents = Incident.objects.exclude(incident_status='RESOLVED')
     context = {'all_incidents' : all_incidents }
     return render(request, 'callcentre/callcentre_home.html', context)
