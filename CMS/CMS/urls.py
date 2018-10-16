0a0213f33c8a966ff99f25fb3c79f92f13c19c16
Simeng Han <31760413+ShirleyHan6@users.noreply.github.com>
2018-10-16 23:52:24 +0800

    Update urls.py

--- CMS/CMS/urls.py
+++ CMS/CMS/urls.py
@@ -6,14 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('map.urls', namespace='map')),
     path('callcentre/', include('callcentre.urls', namespace='callcentre')),
-<<<<<<< HEAD
     path('department/', include('department.urls', namespace='department')),
     path('statustrack/', include('statusTrack.urls', namespace='statustrack')),
     path('infodistribution/', include('infodistribution.urls', namespace='infodistribution')),
+    path('incidentCreation/', include('incidentCreation.urls', namespace='incidentCreation')),
 ]
-=======
-    # path('department/', include('department.urls', namespace='department')),
-    path('statustrack/', include('statusTrack.urls', namespace='statustrack')),
-    path('infodistribution/', include('infodistribution.urls', namespace='infodistribution')),
-]
->>>>>>> e1a117cd58a5e32619dfefd31599f1712a0a7f8e
