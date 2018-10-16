e197cc8d9de37bf81d913efba96a205639d8587e
mengyan-FU <mfu003@e.ntu.edu.sg>
2018-10-16 21:52:04 +0800

    change

--- CMS/callcentre/models.py
+++ CMS/callcentre/models.py
@@ -3,15 +3,16 @@ from django.urls import reverse
 from utilities.incidentstatus import IncidentStatus
 from utilities.incidenttype import IncidentType
 from utilities.region import Region
+from django.utils import timezone
 
 
 class Incident(models.Model):
-    caller_name = models.CharField(max_length=50)
-    mobile_number = models.CharField(max_length=8)
-    incident_time = models.DateTimeField()
-    incident_location = models.CharField(max_length=100)
+    caller_name = models.CharField(max_length=50, default='NULL')
+    mobile_number = models.CharField(max_length=8, default='NULL')
+    incident_time = models.DateTimeField(default=timezone.now)
+    incident_location = models.CharField(max_length=100, default='NULL')
     incident_department = models.CharField(max_length=100, default='NULL')
-    incident_region = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in Region])
-    incident_category = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentType])
-    incident_status = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentStatus])
-    incident_description = models.CharField(max_length=400)
+    incident_region = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in Region], default='NULL')
+    incident_category = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentType], default='NULL')
+    incident_status = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentStatus], default='NULL')
+    incident_description = models.CharField(max_length=400, default='NULL')
