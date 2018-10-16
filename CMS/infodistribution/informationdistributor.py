f48f3bec3b42caa29a87a4322b192e0deb0c6b72
Emil Luusua <emilluusua@Borta.local>
2018-10-16 13:44:16 +0800

    Simplified InformationDistributor

--- CMS/infodistribution/informationdistributor.py
+++ CMS/infodistribution/informationdistributor.py
@@ -1,8 +1,6 @@
-from utilities.incidentstatus import IncidentStatus
 from .statusreportgenerator import StatusReportGenerator
 from .socialmediaalerter import SocialMediaAlerter
 from .dispatcher import Dispatcher
-
 
 
 class InformationDistributor:
@@ -12,26 +10,12 @@ class InformationDistributor:
     """
 
     def __init__(self):
-        """
-        Only incidents with the status NEW shall be dispatched to a department
-        and needs to be notified to the status report generator.
-        """
-        self.new_incident_observers = [Dispatcher(),
-                                       StatusReportGenerator(1800.0)]
-
-        """
-        All incident status updates shall be included in social medias alerts.
-        """
-        self.incident_status_observers = [SocialMediaAlerter()]
-
+        self.observers = [Dispatcher(), SocialMediaAlerter(),
+                          StatusReportGenerator(1800.0)]
 
     def distribute(self, message):
         """
-        Distributes the given message to all observers of that Incident Status.
+        Distributes the given message to all observers.
         """
-        if message.incident_status == IncidentStatus.NEW:
-            for observer in self.new_incident_observers:
-                observer.notify(message)
-
-        for observer in self.incident_status_observers:
+        for observer in self.observers:
             observer.notify(message)
