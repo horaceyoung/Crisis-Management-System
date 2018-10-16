98a59be0b751dac6f6a161abe234c79b0520fb71
Simeng Han <31760413+ShirleyHan6@users.noreply.github.com>
2018-10-16 13:48:34 +0800

    Update models.py

--- CMS/callcentre/models.py
+++ CMS/callcentre/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class Incident(models.Model):
     caller_name = models.CharField(max_length=50)
-	mobile_number = models.CharField(max_length=8)
+    mobile_number = models.CharField(max_length=8)
     incident_time = models.CharField(max_length=100)
     incident_location = models.CharField(max_length=100)
     incident_category = models.CharField(max_length=100)
