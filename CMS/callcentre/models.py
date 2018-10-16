99870249d35c15c6aa939a9469f32f0845b2773a
mengyan-FU <mfu003@e.ntu.edu.sg>
2018-10-16 18:46:15 +0800

    change

--- CMS/callcentre/models.py
+++ CMS/callcentre/models.py
@@ -10,11 +10,8 @@ class Incident(models.Model):
     mobile_number = models.CharField(max_length=8)
     incident_time = models.DateTimeField()
     incident_location = models.CharField(max_length=100)
-<<<<<<< HEAD
     incident_department = models.CharField(max_length=100, default='NULL')
-=======
     incident_region = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in Region])
     incident_category = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentType])
     incident_status = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentStatus])
     incident_description = models.CharField(max_length=400)
->>>>>>> 865a9a6c1fb4cde7a523407ebfd2f114e489304e
