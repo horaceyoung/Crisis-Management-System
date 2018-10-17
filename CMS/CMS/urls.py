678b7f762089aaaf09a2f4f1819218a9ca5ffac7
Emil Luusua <emilluusua@Borta.local>
2018-10-17 13:00:45 +0800

    Removed all references of department and the infodistribution app

--- CMS/CMS/urls.py
+++ CMS/CMS/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('map.urls', namespace='map')),
     path('callcentre/', include('callcentre.urls', namespace='callcentre')),
-    path('department/', include('department.urls', namespace='department')),
     path('statustrack/', include('statusTrack.urls', namespace='statustrack')),
-    path('infodistribution/', include('infodistribution.urls', namespace='infodistribution')),
     path('incidentCreation/', include('incidentCreation.urls', namespace='incidentCreation')),
 ]
--- CMS/infodistribution/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'department'
-
-urlpatterns = [
-    path('', views.department_home, name='department_home'),
-]
