38224a300c5379442e1389269341f82cda8923f3
Emil Luusua <emilluusua@Borta.local>
2018-10-17 14:13:22 +0800

    Added message counter to subsys3 components

--- CMS/infodistribution/tests.py
+++ CMS/infodistribution/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .informationdistributor import InformationDistributor
 from .statusreportgenerator import StatusReportGenerator, KeyIndicators
 from callcentre.models import Incident
 from utilities.message import Message
@@ -6,6 +7,7 @@ from utilities.incidentstatus import IncidentStatus
 from utilities.incidenttype import IncidentType
 from utilities.region import Region
 from datetime import datetime
+
 
 class InformationDistributorTest(TestCase):
     def setUp(self):
@@ -16,6 +18,12 @@ class InformationDistributorTest(TestCase):
         incident1.save()
         self.message = Message(incident1.id, incident1.incident_status)
 
+    def test_message_receivement(self):
+        infodist = InformationDistributor()
+        infodist.distribute(self.message)
+
+        for observer in infodist.observers:
+            self.assertEqual(1, observer.messages_received, 'Component should have received exactly 1 message')
 
 
 class KeyIndicatorsTest(TestCase):
