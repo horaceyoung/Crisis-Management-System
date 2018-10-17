e9795b3fece9370c7c7e6893e784c8106f7fe38b
Emil Luusua <emilluusua@Borta.local>
2018-10-17 17:47:11 +0800

    Updated all infodist files to correspond to previous master

--- CMS/infodistribution/dispatcher.py
+++ CMS/infodistribution/dispatcher.py
@@ -4,24 +4,24 @@ from utilities.incidentstatus import IncidentStatus
 from utilities.region import Region
 from callcentre.models import Incident
 
+
 class Dispatcher:
     def __init__(self):
+        self.messages_received = 0
         self.distro = distributeMessage()
-        self.prev_key_indicators = KeyIndicators()
-        self.key_indicators = KeyIndicators()
         self.singapore_power_phone = "1234567"
         self.SCDF_phone = "7654321"
-    
+
     def notify(self, message):
+        self.messages_received += 1
         incident = Incident.objects.get(pk=message.incident_id)
         # organise message to be fit for sms
-        self.distro.sendSMS(message)
+        self.distro.sendSMS(self.generate_sms(incident))
 
-    def generatre_SMS(self):
-    sms = "Test sms"
+    def generate_sms(self, incident):
+        sms = "Test sms"
 
-    if self.key_indicators.reported_incidents_by_type() == "Gas Leak Control"
-        self.distro.sendSMS(sms,singapore_power_phone)
-    elif
-        self.distro.sendSMS(sms,SCDF_phone)
-
+        if incident.incident_category == "Gas Leak Control":
+            self.distro.sendSMS(sms, self.singapore_power_phone)
+        else:
+            self.distro.sendSMS(sms, self.SCDF_phone)
--- CMS/infodistribution/socialmediaalerter.py
+++ CMS/infodistribution/socialmediaalerter.py
@@ -3,9 +3,11 @@ from .messageDistro import distributeMessage
 
 class SocialMediaAlerter:
     def __init__(self):
+        self.messages_received = 0
         self.distro = distributeMessage
 
     def notify(self, message):
+        self.messages_received += 1
         # organise message to be fit for twitter
         self.distro.sendTweet(message)
         # organize message to be for sms
