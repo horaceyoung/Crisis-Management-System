fd4d74a2491c6a7de8622c1dfcc3473cd5085051
sp00mm <atarang1@uwyo.edu>
2018-10-17 13:36:42 +0800

    Update fix error, still not working

--- CMS/infodistribution/dispatcher.py
+++ CMS/infodistribution/dispatcher.py
@@ -7,21 +7,16 @@ from callcentre.models import Incident
 class Dispatcher:
     def __init__(self):
         self.distro = distributeMessage()
-        self.prev_key_indicators = KeyIndicators()
-        self.key_indicators = KeyIndicators()
         self.singapore_power_phone = "1234567"
         self.SCDF_phone = "7654321"
+        self.sms = "test sms"
     
     def notify(self, message):
         incident = Incident.objects.get(pk=message.incident_id)
-        # organise message to be fit for sms
-        self.distro.sendSMS(message)
+        if IncidentType == "Gas Leak Control":
+            self.distro.sendSMS(self.sms, self.singapore_power_phone)
+        else:
+            self.distro.sendSMS(self.sms, self.SCDF_phone)
 
-    def generatre_SMS(self):
-    sms = "Test sms"
 
-    if self.key_indicators.reported_incidents_by_type() == "Gas Leak Control"
-        self.distro.sendSMS(sms,singapore_power_phone)
-    elif
-        self.distro.sendSMS(sms,SCDF_phone)
 
