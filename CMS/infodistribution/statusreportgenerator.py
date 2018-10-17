0627ab1e4b3ae67b978b053a5ed2ffb1959f8752
Emil Luusua <emilluusua@Borta.local>
2018-10-17 11:54:18 +0800

    Added comments to status report generator

--- CMS/infodistribution/statusreportgenerator.py
+++ CMS/infodistribution/statusreportgenerator.py
@@ -8,6 +8,13 @@ from datetime import datetime
 
 
 class StatusReportGenerator:
+    """
+    Generates status reports for the prime minister containing key indicators
+    and trends (calculated from the key indicators of the previous report).
+
+    interval: indicates how often status reports are generated.
+    """
+
     def __init__(self, interval):
         self.distro = distributeMessage()
         self.interval = interval
@@ -18,6 +25,9 @@ class StatusReportGenerator:
         Timer(interval, self.generate_report).start()  # Schedule first report
 
     def notify(self, message):
+        """
+        Updates key indicators according to the incident update contained in the given message.
+        """
         incident = Incident.objects.get(pk=message.incident_id)
         if message.incident_status == IncidentStatus.NEW:
             self.key_indicators.reported_incidents[incident.incident_category] += 1
@@ -27,12 +37,15 @@ class StatusReportGenerator:
             self.key_indicators.resolved_incidents += 1
 
     def generate_report(self):
+        """
+        Generates a status report based on key indicatorsm, sends it by email to the prime minister and
+        resets key indicators.
+        """
         # Schedule next report
         Timer(self.interval, self.generate_report).start()
 
         title = 'Incident Status Report ' + str(datetime.now())
-        report = 'Number of reported incidents of type\n'
-        report += self.key_indicators.reported_incidents_by_type()
+        report = self.key_indicators.reported_incidents_by_type() + '\n'
         report += self.key_indicators.ongoing_incidents() + '\n'
         report += self.key_indicators.mean_resolution_time() + '\n'
         report += self.key_indicators.trending_incident_type(self.prev_key_indicators) + '\n'
@@ -40,12 +53,16 @@ class StatusReportGenerator:
 
         self.distro.sendEmail(title, report, self.prime_minister_address)
 
-        # Reset statistics
+        # Reset key indicators
         self.prev_key_indicators = self.key_indicators
         self.key_indicators = KeyIndicators()
 
 
 class KeyIndicators:
+    """
+    Contains statistics used to calculate key indicators and trends.
+    Can calculate the indicators and trends, which are returned as human-readable strings.
+    """
     def __init__(self):
         self.reported_incidents = {key: 0 for key in IncidentType}
         self.affected_regions = {key: 0 for key in Region}
@@ -53,27 +70,42 @@ class KeyIndicators:
         self.total_resolution_time = 0.0
 
     def reported_incidents_by_type(self):
-        res = ''
+        """
+        Calculates the number of incidents reported of each type.
+        """
+        res = 'Number of reported incidents of type'
         for tag in IncidentType:
-            res += '\t- ' + tag.value + ': ' + self.key_indicators.reported_incidents[tag] + '.\n'
+            res += '\n\t- ' + tag.value + ': ' + self.key_indicators.reported_incidents[tag] + '.'
 
     def ongoing_incidents(self):
+        """
+        Calculates the number of incidents which have not yet been resolved.
+        """
         ongoing = sum(self.reported_incidents.values()) - self.resolved_incidents
         return 'Number of incidents which are still ongoing: ' + ongoing + '.'
 
     def mean_resolution_time(self):
+        """
+        Calculates the mean time for an incident to become resolved.
+        """
         mtr = self.total_resolution_time / self.resolved_incidents
         return 'Mean time of incident resolution: ' + str(mtr) + '.'
 
     def trending_incident_type(self, prev):
-        increase, type = self.__largest_derivative(self.reported_incidents, prev.reported_incidents, IncidentType)
+        """
+        Calculates the incident type for which the number of reports are currently increasing the most.
+        """
+        type, increase = self.__largest_derivative(self.reported_incidents, prev.reported_incidents, IncidentType)
         if increase == 0:
             return 'The number of reported incidents is not increasing in any category.'
         else:
             return 'The number of reported incidents in the ' + type.value + ' category are increasing the fastest.'
 
     def trending_region(self, prev):
-        increase, region = self.__largest_derivative(self.affected_regions, prev.affected_regions, Region)
+        """
+        Calculates the region in which the number of incident reports are currently increasing the most.
+        """
+        region, increase = self.__largest_derivative(self.affected_regions, prev.affected_regions, Region)
         if increase == 0:
             return 'The number of reported incidents is not increasing in any region.'
         else:
@@ -81,6 +113,9 @@ class KeyIndicators:
 
     @staticmethod
     def __largest_derivative(dict1, dict2, keys):
+        """
+        Returns the key in keys which maximizes dict2[key] - dict1[key] and the value.
+        """
         best_derivative = 0
         best_key = None
         for key in keys:
@@ -88,4 +123,4 @@ class KeyIndicators:
                 best_derivative = dict2[key] - dict1[key]
                 best_key = key
 
-        return best_derivative, best_key
+        return best_key, best_derivative
