88ad09fe2c96d00b8b7254b1e4ae3c9988496e03
Emil Luusua <emilluusua@Borta.local>
2018-10-17 13:53:12 +0800

    Fixed errors in dispatcher

--- CMS/infodistribution/dispatcher.py
+++ CMS/infodistribution/dispatcher.py
@@ -7,21 +7,19 @@ from callcentre.models import Incident
 class Dispatcher:
     def __init__(self):
         self.distro = distributeMessage()
-        self.prev_key_indicators = KeyIndicators()
-        self.key_indicators = KeyIndicators()
         self.singapore_power_phone = "1234567"
         self.SCDF_phone = "7654321"
     
     def notify(self, message):
         incident = Incident.objects.get(pk=message.incident_id)
         # organise message to be fit for sms
-        self.distro.sendSMS(message)
+        self.distro.sendSMS(self.generate_sms(incident))
 
-    def generatre_SMS(self):
-    sms = "Test sms"
+    def generate_sms(self, incident):
+        sms = "Test sms"
 
-    if self.key_indicators.reported_incidents_by_type() == "Gas Leak Control"
-        self.distro.sendSMS(sms,singapore_power_phone)
-    elif
-        self.distro.sendSMS(sms,SCDF_phone)
+        if incident.incident_category == "Gas Leak Control":
+            self.distro.sendSMS(sms, self.singapore_power_phone)
+        else:
+            self.distro.sendSMS(sms, self.SCDF_phone)
 
