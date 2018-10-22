13318db3eadd0f358131ab70e12d6375a38916dd
Simeng Han <31760413+ShirleyHan6@users.noreply.github.com>
2018-10-22 12:44:01 +0800

    Update models.py

--- CMS/incidentCreation/models.py
+++ CMS/incidentCreation/models.py
@@ -7,8 +7,7 @@ sys.path.append(os.path.abspath(os.path.join('..', 'callcentre')))
 
 from callcentre.models import Incident
 
-
 class ContactForm2(forms.ModelForm):
     class Meta:
-        model=Incident                    #表明这个表单继承自叫 RemarkForm的模型
-        fields=('caller_name','mobile_number','incident_time', 'incident_location', 'incident_category')           #指定表单继承模型的哪些属性
+        model=Incident                   
+        fields=('caller_name','mobile_number', 'incident_location', 'incident_category') 
