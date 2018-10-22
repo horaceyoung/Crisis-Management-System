0c68fc30aa37b85714ebfce22dc1f8c01f5f6abc
Simeng Han <31760413+ShirleyHan6@users.noreply.github.com>
2018-10-22 18:01:26 +0800

    Update models.py

--- CMS/callcentre/models.py
+++ CMS/callcentre/models.py
@@ -5,7 +5,6 @@ from utilities.incidenttype import IncidentType
 from utilities.region import Region
 from django.utils import timezone
 
-
 CATEGORY_CHOICES = [    # Note square brackets.
     ("Emergency Ambulance", "Emergency Ambulance"), 
     ("Rescue & Evacuation", "Rescue & Evacuation"),
@@ -13,6 +12,31 @@ CATEGORY_CHOICES = [    # Note square brackets.
     ("Gas Leak Control", "Gas Leak Control"),        
 ]
 
+TYPE_CHOICES = [
+    ("--------Emergency Situations--------", "--------Emergency Situations--------"),
+    ("----Natural Hazards----", "----Natural Hazards----"),
+    ("Fire", "Fire"),
+    ("Haze", "Haze"),
+    ("Tsunami", "Tsunami"),
+    ("Typhoon", "Typhoon"),
+    ("Earthquake", "Earthquake"),
+    ("Earthquake Aftershocks", "Earthquake Aftershocks"),
+    ("----Epidemic----", "----Epidemic----"),
+    ("Dengue", "Dengue"),
+    ("HIV/AIDS", "HIV/AIDS"),
+    ("Bird-flu", "Bird-flu"),
+    ("Zika", "Zika"),
+    ("----Traffic Accidents----","----Traffic Accidents----"),
+    ("Single Car Accidents","Single Car Accidents"),
+    ("Two Car Collisions","Two Car Collisions"),
+    ("Multiple Vehicle Pile-up","Multiple Vehicle Pile-up"),
+    ("----Incidents within Crowded Areas----", "----Incidents within Crowded Areas----"),
+    ("Terrorist Attacks","Terrorist Attacks"),
+    ("Mass Shooting","Mass Shooting"),
+    ("Other Useful Information", "Other Useful Information"),
+    ("Weather", "Weather"),
+    ("Civil Defense Shelters", "Civil Defense Shelters"),
+]
 class Incident(models.Model):
     caller_name = models.CharField(max_length=50, default='NULL')
     mobile_number = models.CharField(max_length=8, default='NULL')
@@ -21,5 +45,7 @@ class Incident(models.Model):
     incident_department = models.CharField(max_length=100, default='NULL')
     incident_region = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in Region], default='NULL')
     incident_category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='NULL')
-    incident_status = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentStatus], default='NULL')
+    incident_type = models.CharField(max_length=100, choices=TYPE_CHOICES, default='NULL')
+    incident_status = models.CharField(max_length=100, choices=[(tag, tag.value) for tag in IncidentStatus], default='NEW')
     incident_description = models.CharField(max_length=400, default='NULL')
+
