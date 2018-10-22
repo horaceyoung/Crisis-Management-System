1b168af34d3f4210132d6d40a257bf7e7293e039
Simeng Han <31760413+ShirleyHan6@users.noreply.github.com>
2018-10-22 19:54:15 +0800

    Update views.py

--- CMS/incidentCreation/views.py
+++ CMS/incidentCreation/views.py
@@ -12,14 +12,15 @@ def incidentCreation(request):
 	if request.method=='GET':
 		form = ContactForm2(request.GET)											
 		if form.is_valid():
-			print("The form is valid\n\n\n\n")
+			# print("The form is valid\n\n\n\n")
 			incident = Incident()
 			incident.caller_name = form.cleaned_data['caller_name']
 			incident.mobile_number = form.cleaned_data['mobile_number']
 			incident.incident_location = form.cleaned_data['incident_location']
 			incident.incident_category = form.cleaned_data['incident_category']
+			incident.incident_type = form.cleaned_data['incident_type']
 			incident.save()
-			print("A new incident is saved\n\n\n\n")
+			# print("A new incident is saved\n\n\n\n")
 			return render_to_response('callcentre/incidentCreation.html',{'form':form},RequestContext(request))
 		else:
 			form=ContactForm2()
