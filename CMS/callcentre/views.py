f8cc21b94323f4f1814f2a8ea2d19a076e7029d4
Simeng Han <31760413+ShirleyHan6@users.noreply.github.com>
2018-10-30 10:03:13 +0800

    Update views.py

--- CMS/callcentre/views.py
+++ CMS/callcentre/views.py
@@ -3,12 +3,12 @@ from .models import Incident
 
 
 def callcentre_home(request):
-    all_unsolved_incidents = Incident.objects.exclude(incident_status='RESOLVED').order_by('incident_time')
+    all_unsolved_incidents = Incident.objects.exclude(incident_status='Resolved').order_by('incident_time')
     context = {'all_unsolved_incidents' : all_unsolved_incidents }
     return render(request, 'callcentre/callcentre_home.html', context)
 
 def callcentre_history(request):
-    incident_history = Incident.objects.filter(incident_status='RESOLVED')
+    incident_history = Incident.objects.filter(incident_status='Resolved')
     context = {'incident_history' : incident_history }
     return render(request, 'callcentre/callcentre_history.html', context)
 
