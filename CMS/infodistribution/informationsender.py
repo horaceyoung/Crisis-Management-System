d045672a68f6f16a10d4872d59e34e777029717d
mengyan-FU <mfu003@e.ntu.edu.sg>
2018-10-31 13:22:28 +0800

    Update on incident display

--- CMS/infodistribution/informationsender.py
+++ CMS/infodistribution/informationsender.py
@@ -1,12 +1,58 @@
+from twilio.rest import Client
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import tweepy
+
+CONSUMER_KEY ="ojjWC47FsOrdkKiQxfITL9FwC"
+CONSUMER_SECRET = "2pQqgJvVjcczubPHjiBxEKkYZFjWQ4gOoqiTV0u1phE2UyY9Da"
+ACCESS_KEY = "1053928436874395648-UM3wmevzmLOJZsQYIt2WvHj64Y5qEY"
+ACCESS_SECRET = "8veJe3ZLy382zYthP1gt6k0rh6aMnTt3HZ4OoD0FcMBC0"
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+twitterApi = tweepy.API(auth)
+
+senderEmail = '8paxemailservice@gmail.com'
+password = '-?.8JG,/zrazT)x*'
+
+
+account_sid = "AC911c8d07cb467d8f69f4a933362de87e"
+auth_token  = "63911216647d96067015c43c4f42eac5"
+
+client = Client(account_sid, auth_token)
+
+
 class InformationSender:
     def send_sms(self, sms, phoneNumber):
-        print("SMS sent")
-        # smsAPI send sms(self.message)
+        message = client.messages.create(
+            to=phoneNumber,
+            from_="+12674788252",
+            body=sms)
+
+        print(message.sid)
+        print("SMS to " + phoneNumber + " sent.")
+        del message
 
     def send_email(self, subject, email, recipient):
-        print("Email sent")
-        # emailAPI send email(self.email)
+        msg = MIMEMultipart()
+        msg['From'] = senderEmail
+        msg['To'] = recipient
+        msg['Subject'] = subject
 
-    def send_tweet(self, tweet, twitterUser):
-        print("Tweet sent")
-        # twitterAPI send tweet(self.tweet)
+        msg.attach(MIMEText(email, 'plain'))
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(senderEmail, password)
+        text = msg.as_string()
+        server.sendmail(senderEmail, recipient, text)
+        server.quit()
+        print("Email to " + recipient + " sent.")
+        del msg
+        del text
+
+    def send_tweet(self, tweet):
+        twitterApi.update_status(tweet)
+        print(tweet + " sent.")
+
--- CMS/infodistribution/socialmediaalerter.py
+++ CMS/infodistribution/socialmediaalerter.py
@@ -1,17 +1,18 @@
 from .informationsender import InformationSender
+from callcentre.models import Incident
 
 
 class SocialMediaAlerter:
     def __init__(self):
         self.messages_received = 0
         self.distro = InformationSender()
-        self.twitter_user = 'SingaporeCMS'
-        self.phone_numbers = [12341234, 12341235]
+        self.phone_numbers = ['+6583980512', '+6583980512']
 
     def notify(self, message):
         self.messages_received += 1
-        # organise message to be fit for twitter
-        self.distro.send_tweet(message, self.twitter_user)
-        # organize message to be for sms
+        alert = "Alert: " + Incident.incident_type
+        alert += " in " + Incident.incident_region
+        alert += " at " + Incident.incident_time
+        self.distro.send_tweet(alert)
         for number in self.phone_numbers:
-            self.distro.send_sms(message, number)
+            self.distro.send_sms(alert, number)
