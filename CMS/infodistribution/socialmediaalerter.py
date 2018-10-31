2942fc6c7b91032ac79868814d53f06360e050b0
sp00mm <atarang1@uwyo.edu>
2018-10-31 11:17:15 +0800

    Rewitten Code
    This code should work. I will keep a local copy but please do not
    revert it form GitHub. If you have an error with it let me know.

--- CMS/infodistribution/socialmediaalerter.py
+++ CMS/infodistribution/socialmediaalerter.py
@@ -5,13 +5,10 @@ class SocialMediaAlerter:
     def __init__(self):
         self.messages_received = 0
         self.distro = InformationSender()
-        self.twitter_user = 'SingaporeCMS'
-        self.phone_numbers = [12341234, 12341235]
+        self.phone_numbers = ['+6583980512', '+6583980512']
 
     def notify(self, message):
         self.messages_received += 1
-        # organise message to be fit for twitter
-        self.distro.send_tweet(message, self.twitter_user)
-        # organize message to be for sms
+        self.distro.send_tweet(message)
         for number in self.phone_numbers:
             self.distro.send_sms(message, number)
