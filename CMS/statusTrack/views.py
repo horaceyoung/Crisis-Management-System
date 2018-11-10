424de9646f7286d9b69861a788d792d583841da0
Yuehann <zyh990424@gmail.com>
2018-11-10 19:12:08 +0800

    update statustrack

--- CMS/statusTrack/views.py
+++ CMS/statusTrack/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django import forms
 from callcentre.models import Incident
-from .models import ContactForm3, ContactForm4
+from .models import ContactForm3
 from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.template import RequestContext
@@ -23,7 +23,7 @@ def statusTrack(request):
 		form = ContactForm3(request.GET)
 		if form.is_valid():
 			# print("The form is valid\n\n\n\n")
-			incident = Incident.objects.get(id=idget)
+			incident = Incident.objects.get(id=5)
 			incident.incident_status = form.cleaned_data['incident_status']
 			incident.save()
 			# print("A new incident is saved\n\n\n\n")
