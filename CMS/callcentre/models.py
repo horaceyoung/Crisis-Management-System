44c4bc0744b9ff02e11e1c251b53a38232269179
Yuehann <zyh990424@gmail.com>
2018-11-13 15:20:54 +0800

    update

--- CMS/callcentre/models.py
+++ CMS/callcentre/models.py
@@ -4,8 +4,11 @@ from utilities.incidentstatus import IncidentStatus
 from utilities.incidenttype import IncidentType
 from utilities.region import Region
 from django.utils import timezone
+from datetime import datetime
+import pytz
 
-# timezone.activate(pytz.timezone("Asia/Singapore"))
+tz = pytz.timezone("Asia/Singapore")
+timezone.activate(pytz.timezone("Asia/Singapore"))
 
 REGION_CHOICES = [
     ("North West","North West"),
@@ -56,7 +59,7 @@ STATUS_UPDATE = [
 class Incident(models.Model):
     caller_name = models.CharField(max_length=50, default='NULL',blank=True)
     mobile_number = models.CharField(max_length=8, default='NULL',blank=True)
-    incident_time = models.DateTimeField(default=timezone.now)
+    incident_time = models.DateTimeField(default=timezone.now())
     incident_location = models.CharField(max_length=100, default='NULL',blank=True)
     incident_department = models.CharField(max_length=100, default='NULL')
     incident_region = models.CharField(max_length=100, choices=REGION_CHOICES, default='NULL')
